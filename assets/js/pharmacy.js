0d9618b31be351061918c7e8d4f6e57516b54043
rahul <rahul.122293@gmail.com>
2020-02-02 03:59:50 +0530

    .

--- assets/js/pharmacy.js
+++ assets/js/pharmacy.js
@@ -2,4 +2,7 @@ function allPrescriptions(){
     document.getElementById('all-prescription').classList.remove('d-none');
 }
 
-auth.onAuthStateChanged()
+auth.onAuthStateChanged(user=>{
+    if(user) console.log(user);
+    else console.log("err")
+})
