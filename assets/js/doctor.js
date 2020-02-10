6123cb3f691f3d00ccd3772d89f11de9555fbb47
Debraj Sengupta <theuniqueraj@gmail.com>
2020-02-10 06:08:32 +0530

    5
    5

--- assets/js/doctor.js
+++ assets/js/doctor.js
@@ -207,7 +207,7 @@ if (jobView) {
         .then(snapshot => {
             snapshot.forEach(doc => {
                 jobView.innerHTML += `
-                <div class="col-5 align-self-center">
+                <div class="col-5 align-self-center rendered-patient">
                     <div class="card text-change bg-light mb-3 mx-2 mt-2" style="max-width: 40rem; height: auto;">
                         <div class="card-header">Consultation with ${doc.data().patientName} </div>
                         <div class="card-body">
@@ -224,4 +224,84 @@ if (jobView) {
             });
         })
     })
+}
+
+
+
+function searchPatient(){
+    var render_static = document.querySelectorAll('.render-static');
+    for (var i = 0; i < render_static.length; i++) {
+        render_static[i].classList.add('d-none');
+     }
+    var rendered_patient = document.querySelectorAll('.rendered-patient');
+    for (var i = 0; i < rendered_patient.length; i++) {
+        rendered_patient[i].classList.add('d-none');
+     }
+    document.querySelector('.render-loader').classList.remove('d-none');
+    setTimeout(async ()=>{
+        var patient = document.querySelector('#patientSearchID').value;
+        //console.log (typeof patient);
+        var jobView = document.getElementById('patients');
+        if(patient !== "")
+        {
+        await db.collection('PatientProfile').where('patientId','==',patient)
+        .get()
+        .then(snapshot => {
+            console.log(snapshot.docs[0]);
+            jobView.innerHTML += `
+            <div class="col-5 align-self-center searched-patient">
+                <div class="card text-change bg-light mb-3 mx-2 mt-2" style="max-width: 40rem; height: auto;">
+                    <div class="card-header">Consultation with ${snapshot.docs[0].data().patientName} </div>
+                    <div class="card-body">
+                    <h6 class="card-title"><span class="text-muted">Patient ID: ${snapshot.docs[0].data().patientId}</span></h6>
+                      <p class="card-text text-change text-left justify-content">
+                            <span class="font-weight-normal">Mobile Number:${snapshot.docs[0].data().mobno} </span><br/>
+                            <span class="font-weight-normal">Email ID : ${snapshot.docs[0].data().email} </span><br/>
+                            <span class="font-weight-normal">Address: ${snapshot.docs[0].data().address} </span>
+                            <div class="text-right"><span class="btn btn-info btn-md" onclick="toggleFab()">Chat Now</span><br/></div>
+                        </p>
+                    </div>
+                  </div>
+            </div>`;
+        })
+        .catch(async error=>{
+            console.log(error);
+            jobView.innerHTML += `
+            <h3 class="text-light mb-2 text-center searched-error">Sorry no patient with entered ID found in the database!</h3>`
+            await setTimeout(()=>{
+                var rendered_patient = document.querySelectorAll('.rendered-patient');
+                for (var i = 0; i < rendered_patient.length; i++) {
+                    rendered_patient[i].classList.remove('d-none');
+                 }
+                var searched_error = document.querySelectorAll('.searched-error');
+                for (var i = 0; i < searched_error.length; i++) {
+                     searched_error[i].classList.add('d-none');
+                  }
+            },3000);
+        });
+        var render_static = document.querySelectorAll('.render-static');
+        for (var i = 0; i < render_static.length; i++) {
+            render_static[i].classList.remove('d-none');
+            }
+        document.querySelector('.render-loader').classList.add('d-none');
+        }
+        else{
+            var render_static = document.querySelectorAll('.render-static');
+            for (var i = 0; i < render_static.length; i++) {
+                render_static[i].classList.remove('d-none');
+                }
+            document.querySelector('.render-loader').classList.add('d-none');
+            var searched_patient = document.querySelectorAll('.searched-patient');
+            for (var i = 0; i < searched_patient.length; i++) {
+                searched_patient[i].classList.add('d-none');
+             }
+            var searched_error = document.querySelectorAll('.searched-error');
+            for (var i = 0; i < searched_error.length; i++) {
+                searched_error[i].classList.add('d-none');
+             }
+            for (var i = 0; i < rendered_patient.length; i++) {
+                rendered_patient[i].classList.remove('d-none');
+             }   
+        }
+    },2000);
 }
